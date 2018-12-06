41b89deadcf3b082dab9fd6e9fb486a947a1f73f
guanming <975402925@qq.com>
2018-12-06 21:22:50 +0800

    生产环境构建-----

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import _ from 'lodash';
 import './style.css'
 import printMe from './print.js';
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
 function component() {
   var element = document.createElement('div');
   var btn = document.createElement('button');
@@ -19,3 +23,10 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+ if (module.hot) {
+   module.hot.accept('./print.js', function() {
+     console.log('Accepting the updated printMe module!');
+     printMe();
+   })
+ }
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
-     aapp: './src/index.js',
-     print: './src/print.js'
+     app: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+      contentBase: './dist',
+      hot: true
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
   module:{
     rules:[
@@ -44,6 +50,8 @@ module.exports = {
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Output Management'
-    })
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ],
 };
